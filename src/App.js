8176b8f141f1e57c2120096b5c9ab451fa96a460
CGAJAY <jmugendi737@gmail.com>
2024-10-07 09:56:42 +0300

    Updated default files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // src/App.js
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PlayersList from "./PlayerList";
+// import PlayersList from "./PlayerList";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div className="">
+			<header className="">
 				<h1>Football Player Cards</h1>
 			</header>
-			<PlayersList />
+			{/* <PlayersList /> */}
 		</div>
 	);
 }
